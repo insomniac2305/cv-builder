2eefe19a1acd6ccefdfe096e009ae237f25cc987
insomniac2305 <p.doerner@outlook.com>
2023-03-17 20:16:42 +0100

    Reorder

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">Hello World!</div>;
+  }
+}
+
+export default App;
