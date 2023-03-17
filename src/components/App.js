262851e42721e667b09171040df15e59e3629dc7
insomniac2305 <p.doerner@outlook.com>
2023-03-17 21:43:13 +0100

    Add general information block

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import { Component } from "react";
+import General from "./General";
 import "../styles/App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World!</div>;
+    return (
+      <div className="App">
+        <header><h1>CV Builder</h1></header>
+        <General />
+      </div>
+    );
   }
 }
 
