c6b955f51175547465afc640c9776ecfc7ec054f
insomniac2305 <p.doerner@outlook.com>
2023-03-23 18:50:49 +0100

    Transform React classes to functions

--- src/components/General.js
+++ src/components/General.js
@@ -1,78 +1,80 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class General extends Component {
-  constructor(props) {
-    super(props);
+function General() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [location, setLocation] = useState("");
+  const [editMode, setEditMode] = useState(false);
 
-    this.state = {
-      name: "",
-      email: "",
-      phone: "",
-      location: "",
-      editMode: false,
+  const handleChange = (e) => {
+    const stateHandler = {
+      name: setName,
+      email: setEmail,
+      phone: setPhone,
+      location: setLocation,
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.toggleEdit = this.toggleEdit.bind(this);
-    this.onEnterKey = this.onEnterKey.bind(this);
-  }
+    stateHandler[e.target.name](e.target.value);
+  };
 
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  }
+  const toggleEdit = () => {
+    setEditMode(!editMode);
+  };
 
-  toggleEdit() {
-    this.setState((state) => ({
-      editMode: !state.editMode,
-    }));
-  }
+  const onEnterKey = (e) => {
+    e.key === "Enter" && toggleEdit();
+  };
 
-  onEnterKey(e) {
-    e.key === "Enter" && this.toggleEdit();
-  }
-
-  render() {
-    const { name, email, phone, location, editMode } = this.state;
-
-    return (
-      <div className="general">
-        <h2>General information</h2>
-        <div className="actions">
-          <button className="edit" onClick={this.toggleEdit}>
-            {editMode ? "Save" : "Edit"}
-          </button>
-        </div>
-
-        <div className="data-row">
-          <div className="label">Name</div>
-          {!editMode && <div className="data name">{name || "-"}</div>}
-          {editMode && <input type="text" id="name" name="name" value={name} onChange={this.handleChange} onKeyDown={this.onEnterKey} />}
-        </div>
-
-        <div className="data-row">
-          <div className="label">E-Mail</div>
-          {!editMode && <div className="data email">{email || "-"}</div>}
-          {editMode && <input type="text" id="email" name="email" value={email} onChange={this.handleChange} onKeyDown={this.onEnterKey} />}
-        </div>
-
-        <div className="data-row">
-          <div className="label">Phone</div>
-          {!editMode && <div className="data phone">{phone || "-"}</div>}
-          {editMode && <input type="text" id="phone" name="phone" value={phone} onChange={this.handleChange} onKeyDown={this.onEnterKey} />}
-        </div>
-
-        <div className="data-row">
-          <div className="label">Location</div>
-          {!editMode && <div className="data location">{location || "-"}</div>}
-          {editMode && (
-            <input type="text" id="location" name="location" value={location} onChange={this.handleChange} onKeyDown={this.onEnterKey} />
-          )}
-        </div>
+  return (
+    <div className="general">
+      <h2>General information</h2>
+      <div className="actions">
+        <button className="edit" onClick={toggleEdit}>
+          {editMode ? "Save" : "Edit"}
+        </button>
       </div>
-    );
-  }
+
+      <div className="data-row">
+        <div className="label">Name</div>
+        {!editMode && <div className="data name">{name || "-"}</div>}
+        {editMode && (
+          <input type="text" id="name" name="name" value={name} onChange={handleChange} onKeyDown={onEnterKey} />
+        )}
+      </div>
+
+      <div className="data-row">
+        <div className="label">E-Mail</div>
+        {!editMode && <div className="data email">{email || "-"}</div>}
+        {editMode && (
+          <input type="text" id="email" name="email" value={email} onChange={handleChange} onKeyDown={onEnterKey} />
+        )}
+      </div>
+
+      <div className="data-row">
+        <div className="label">Phone</div>
+        {!editMode && <div className="data phone">{phone || "-"}</div>}
+        {editMode && (
+          <input type="text" id="phone" name="phone" value={phone} onChange={handleChange} onKeyDown={onEnterKey} />
+        )}
+      </div>
+
+      <div className="data-row">
+        <div className="label">Location</div>
+        {!editMode && <div className="data location">{location || "-"}</div>}
+        {editMode && (
+          <input
+            type="text"
+            id="location"
+            name="location"
+            value={location}
+            onChange={handleChange}
+            onKeyDown={onEnterKey}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default General;
